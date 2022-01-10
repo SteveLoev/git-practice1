5c6f7727955218fb4626c11d904dbfe1fd7a8856
Steve Loev <steve.loev@gmail.com>
2022-01-10 14:44:23 -0500

    pushing work 3

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,7 @@ public class HelloWorld {
 
     //some text here
     //doing some awesome work 2!
-
+    //doing awesome work 3!
+    //work 3 done and ready to be committed
+    //work 3 ready to be pushed to remote repository
 }
